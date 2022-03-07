82f0b946445071ccb9196126de86a652e3e9f491
Sanjay Saatyaki <Saatyaki.Sanjay@in.bosch.com>
2022-03-07 12:45:03 +0530

    Implementation

--- current_range_calc.py
+++ current_range_calc.py
@@ -1,2 +1,22 @@
+import more_itertools as mit
+
+def  get_range(lst):
+    return [list(group) for group in mit.consecutive_groups(lst)]
+
+def get_reading_count(item, lst):
+    count = 0
+    for i  in range(item[0],item[-1]+1):
+        count = count + lst.count(i)
+    return count
+
 def calc_ranges(lst):
-    return 4
+    range_dict = {}
+    lst.sort()
+    ranges = get_range(lst)
+    for item in ranges:
+        if len(item)!=1:
+            print(item, get_reading_count(item, lst))
+            range_dict.update({(item[0],item[-1]):get_reading_count(item,lst)})
+    return range_dict
+
+calc_ranges([3,3,5,4])
--- current_range_calc.test.py
+++ current_range_calc.test.py
@@ -2,10 +2,19 @@ import unittest
 import current_range_calc
 
 class current_range_calc_test(unittest.TestCase):
-    
-    def test_calc_ranges(self):
-       self.assertTrue(current_range_calc.calc_ranges([3,3,5,4]) ==  4)
 
+    input_lst =[3,3,5,4]
+    def test_calc_ranges(self):
+       self.assertTrue(current_range_calc.calc_ranges(self.input_lst) ==  {(3,5):4})
+      
+    def test_get_range(self):
+      lst = self.input_lst
+      lst.sort()
+      res = current_range_calc.get_range(lst)
+      self.assertTrue([3,4,5] in res)
+    
+    def test_get_reading_count(self):
+      self.assertTrue(current_range_calc.get_reading_count([3,4,5],self.input_lst)==4)
 
 
 if __name__ == '__main__':
