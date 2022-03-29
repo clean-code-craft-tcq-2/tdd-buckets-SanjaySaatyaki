917f7fb56b7d11d261134bb537eeff26588f2233
Sanjay Saatyaki <Saatyaki.Sanjay@in.bosch.com>
2022-03-29 18:17:34 +0530

    Changes for adding new features

--- current_range_calc.py
+++ current_range_calc.py
@@ -1,5 +1,6 @@
 import more_itertools as mit
 import csv
+import random
 
 def  get_range(lst):
     return [list(group) for group in mit.consecutive_groups(lst)]
@@ -29,3 +30,25 @@ def export_dict_to_csv(range_dict):
         for key in range_dict:
             writer.writerow({'Range': str(key)[1:-1].replace(",","-"), 'Readings': range_dict[key]})
         return "Ranges_and_Readings.csv"
+
+def remove_invalid_data(samples,range_value):
+    valid_samples = [sample for sample in samples if sample <= range_value]
+    return valid_samples
+    
+
+def A2D_12BtoAmps_convertor(samples):
+    valid_samples_amps = [round(sample*10/4094) for sample in samples]
+    return valid_samples_amps
+
+def A2D_10BtoAmps_convertor(samples):
+    valid_samples_amps = [abs(round(sample*30/1022) - 15) for sample in samples]
+    return valid_samples_amps
+
+def calc_ranges_from_sensor(samples,sensor):
+    if sensor == "A2D_12B":
+        valid_samples = remove_invalid_data(samples,4094)
+        amps_sample = A2D_12BtoAmps_convertor(valid_samples)
+    elif sensor == "A2D_10B":
+        valid_samples = remove_invalid_data(samples,1022)
+        amps_sample = A2D_10BtoAmps_convertor(valid_samples)
+    return calc_ranges(amps_sample)
